ead12ce723954b7a53881076d1bb240c9757f1ca
Ping-Hsuan-Yu <yu.pinghsuan@gmail.com>
2024-10-18 14:02:37 +0800

    "Revert error throwing and uncomment return statement in Room function"

--- src/app/dashboard/@room/page.tsx
+++ src/app/dashboard/@room/page.tsx
@@ -2,8 +2,8 @@ import { pause } from "@/lib/utils"
 
 export default async function Room(){
     await pause(2000)
-    throw new Error("Fail to fetch room data")
-    // return (
-    //     <div>Room</div>
-    // )
+    // throw new Error("Fail to fetch room data")
+    return (
+        <div>Room</div>
+    )
 }
